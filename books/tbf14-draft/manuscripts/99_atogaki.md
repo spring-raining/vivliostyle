c9cb096178b9dd69b1a2771270ab06d1ddfc0294
spring-raining <harusamex.com@gmail.com>
2023-05-18 23:19:10 +0900

    Update tbf14-draft

--- books/tbf14-draft/manuscripts/99_atogaki.md
+++ books/tbf14-draft/manuscripts/99_atogaki.md
@@ -3,4 +3,37 @@ title: あとがき
 class: afterword
 ---
 
-あとがきだよ
+## あとがき
+
+本書では、複数のVivliostyle Themesを紹介していきました。
+私自身も、一冊の本でこれだけ多様なCSSを用意したことはなかったので、色々と手探りの箇所が多くありました。
+それでも、こうして様々なCSSを破綻なく一冊の本としてまとめることができたという点自体が、Vivliostyleの登場以来9年ほどで得た機能の成熟の結果ではないでしょうか。
+
+また、この本を読んでCSSを書かれた方は、次のステップとして作成したCSSファイルをthemeとしてnpm registry（Vivliostyle Themesを公開しているパッケージ管理システム）に公開することもぜひ検討してもらいたいです。
+これまでCSS自体があまりポータビリティ性を考えて書かれてこなかったこともあるので、いきなりthemeとして公開できるようなCSSを書くことは難しいかもしれません。
+しかし不思議なもので、他の人に使われることを意識してCSSを書くことで、CSSの品質がより向上していくことを実感していくものなのです（大抵は、後で自分が再利用するときに一番助かります）。
+今後は、Vivliostyle Themesを自分で公開する方法についても紹介していこうと思います。
+
+<section id="colophon">
+<h2>CSS組版をマスター！ Vivliostyle Themesレシピ集</h2>
+
+<ul class="editions">
+  <li>
+    <time datetime="2023-05-21">2023年5月21日</time>
+    初版発行
+  </li>
+</ul>
+
+<div class="info">
+  <div>発行</div>
+  <div>Vivliostyleユーザー会</div>
+  <div>編集</div>
+  <div>
+    spring-raining<br />
+    https://harusamex.com<br />
+    harusamex.com@gmail.com
+  </div>
+</div>
+
+© Vivliostyle User Group
+</section>
--- books/tbf14-draft/vivliostyle.config.js
+++ books/tbf14-draft/vivliostyle.config.js
@@ -1,6 +1,6 @@
 /** @type {import('@vivliostyle/cli').VivliostyleConfigSchema} */
 module.exports = {
-  title: 'CSS組版をマスター！ Vivliostyle Themeレシピ集',
+  title: 'CSS組版をマスター！ Vivliostyle Themesレシピ集',
   author: 'spring-raining <harusamex.com@gmail.com>',
   language: 'ja',
   size: 'A4',
@@ -18,11 +18,17 @@ module.exports = {
       path: 'node_modules/www/dist/articles/grid-system/index.html',
     },
     {
+      path: 'node_modules/www/dist/articles/theme-photo-book/index.html',
+    },
+    {
       path: 'node_modules/@spring-raining/vs-theme-photo-book/example/index.md',
       theme: ['../../themes/grid-system', '../../themes/photo-book'],
     },
     {
       path: 'node_modules/www/dist/articles/eal-float-system/index.html',
+    },
+    {
+      path: 'node_modules/www/dist/articles/theme-eal-magazine/index.html',
     },
     {
       path: 'node_modules/@spring-raining/vs-theme-magazine/example/index.md',
@@ -43,6 +49,7 @@ module.exports = {
   workspaceDir: '.vivliostyle',
   includeAssets: [
     'node_modules/www/dist/**/*',
-    'node_modules/@spring-raining/**/*.svg',
+    'node_modules/@spring-raining/vs-theme-magazine/example/assets/**/*',
+    'node_modules/@spring-raining/vs-theme-photo-book/example/assets/**/*',
   ],
 };
