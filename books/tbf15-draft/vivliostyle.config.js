87dcfc1c7abe907b1eb8ff1c4b394d63d719e0d7
spring-raining <harusamex.com@gmail.com>
2023-11-11 19:22:56 +0900

    Support EPUB output for tbf15

--- books/tbf15-draft/vivliostyle.config.js
+++ books/tbf15-draft/vivliostyle.config.js
@@ -1,51 +1,74 @@
 /** @type {import('@vivliostyle/cli').VivliostyleConfigSchema} */
-export default {
+const base = {
   title: 'AstroとVivliostyle CLIでWeb&出版物ハイブリッド開発',
   author: 'spring-raining <harusamex.com@gmail.com>',
   language: 'ja',
   size: 'JIS-B5',
-  entry: [
-    {
-      rel: 'cover',
-      path: 'cover.html',
-      imageSrc: './P1.png',
-      imageAlt: '表紙',
-      pageBreakBefore: 'verso',
-    },
-    { rel: 'contents', path: 'index.html' },
-    'node_modules/www/dist/article/raw/tbf15/00_まえがき/index.html',
-    'node_modules/www/dist/article/raw/tbf15/10_webと出版物のハイブリッド開発とは/index.html',
-    'node_modules/www/dist/article/raw/tbf15/20_node-js-npmを使った開発とファイル構成/index.html',
-    'node_modules/www/dist/article/raw/tbf15/30_原稿を書く/index.html',
-    'node_modules/www/dist/article/raw/tbf15/40_出版物として出力する/index.html',
-    'node_modules/www/dist/article/raw/tbf15/50_cssでデザインする/index.html',
-    'node_modules/www/dist/article/raw/tbf15/90_あとがき/index.html',
-    {
-      rel: 'cover',
-      path: 'back-cover.html',
-      imageSrc: './P4.png',
-      imageAlt: '裏表紙',
-    },
-  ],
-  theme: [
-    {
-      specifier: '../../themes/tbf15/node_modules/@vivliostyle/theme-base',
-      import: 'theme-basic.css',
-    },
-    '../../themes/tbf15',
-  ],
-  output: [
-    './output.pdf',
-    // './output.epub',
-    {
-      path: './book',
-      format: 'webpub',
-    },
-  ],
-  workspaceDir: '.vivliostyle',
-  cover: './P1.png',
   copyAsset: {
-    includes: ['node_modules/www/dist'],
-    excludes: ['node_modules/www/dist/article'],
+    includes: ['node_modules/www/dist/pages/articles/tbf15'],
   },
 };
+
+const entryWithoutCover = [
+  { rel: 'contents', path: 'index.html' },
+  'node_modules/www/dist/article/raw/tbf15/00_preface/index.html',
+  'node_modules/www/dist/article/raw/tbf15/10_what-is-hybrid-development-of-web-and-publications/index.html',
+  'node_modules/www/dist/article/raw/tbf15/20_development-and-file-structure-using-nodejs-and-npm/index.html',
+  'node_modules/www/dist/article/raw/tbf15/30_writing-the-manuscript/index.html',
+  'node_modules/www/dist/article/raw/tbf15/40_output-as-a-publication/index.html',
+  'node_modules/www/dist/article/raw/tbf15/50_design-with-css/index.html',
+  'node_modules/www/dist/article/raw/tbf15/90_afterword/index.html',
+];
+
+const entryWithCover = [
+  {
+    rel: 'cover',
+    path: 'cover.html',
+    imageSrc: './P1.png',
+    imageAlt: '表紙',
+    pageBreakBefore: 'verso',
+  },
+  ...entryWithoutCover,
+  {
+    rel: 'cover',
+    path: 'back-cover.html',
+    imageSrc: './P4.png',
+    imageAlt: '裏表紙',
+  },
+];
+
+const theme = [
+  {
+    specifier: '../../themes/tbf15/node_modules/@vivliostyle/theme-base',
+    import: 'theme-basic.css',
+  },
+  '../../themes/tbf15',
+];
+
+export default [
+  // For digital devices (styled)
+  {
+    ...base,
+    entry: entryWithCover,
+    output: ['output.pdf', 'output-styled.epub'],
+    theme,
+    workspaceDir: '.vivliostyle/styled',
+    cover: './P1.png',
+  },
+  // For digital devices (unstyled)
+  {
+    ...base,
+    entry: entryWithCover,
+    output: ['output-unstyled.epub'],
+    workspaceDir: '.vivliostyle/unstyled',
+    cover: './P1.png',
+  },
+  // For printing
+  {
+    ...base,
+    entry: entryWithoutCover,
+    output: ['output-printing.pdf'],
+    theme: [...theme, 'print.css'],
+    workspaceDir: '.vivliostyle/styled',
+  },
+];
